f4f4e90643d09d9a1f8ffc72489a4dcfa80a5009
Muhammad Bilal Farid <bilaldrosh1@gmail.com>
2025-10-18 01:12:32 +0500

    create Signin class and test it

---
+++ swift_trip_app/lib/screens/Signin.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Signin extends StatefulWidget {
+  const Signin({super.key});
+  @override
+  State<Signin> createState() {
+    return SigninState();
+  }
+}
+
+class SigninState extends State<Signin> {
+  @override
+  Widget build(context) {
+    return Text("Testing Signin Class");
+  }
+}
