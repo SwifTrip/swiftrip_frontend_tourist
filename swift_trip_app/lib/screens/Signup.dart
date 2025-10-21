50a39b40c1d30da77ac2339d5ed75497cb4bb6c4
Muhammad Bilal Farid <bilaldrosh1@gmail.com>
2025-10-21 22:29:31 +0500

    add SignupScreen scaffold with styled AppBar

---
+++ swift_trip_app/lib/screens/Signup.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class SignupScreen extends StatelessWidget {
+  const SignupScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFF0F5FF),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFFF0F5FF),
+        elevation: 0,
+        leading: const Icon(Icons.arrow_back, color: Color(0xFF111827)),
+        title: const Text(
+          'Create Account',
+          style: TextStyle(
+            color: Color(0xFF111827),
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: Container(), 
+    );
+  }
+}
