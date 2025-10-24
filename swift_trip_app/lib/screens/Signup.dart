a71579f75ef6a0a1deabd7bcf697f48ca3acc760
Muhammad Bilal Farid <bilaldrosh1@gmail.com>
2025-10-24 23:11:11 +0500

    Refactor SignupScreen to use stateful widget and controllers

--- swift_trip_app/lib/screens/Signup.dart
+++ swift_trip_app/lib/screens/Signup.dart
@@ -1,7 +1,40 @@
 import 'package:flutter/material.dart';
 
-class SignupScreen extends StatelessWidget {
+class SignupScreen extends StatefulWidget {
   const SignupScreen({super.key});
+
+  @override
+  State<SignupScreen> createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<SignupScreen> {
+  // Form key and controllers
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  late final TextEditingController _firstNameController;
+  late final TextEditingController _lastNameController;
+  late final TextEditingController _emailController;
+  late final TextEditingController _passwordController;
+  late final TextEditingController _confirmPasswordController;
+
+  @override
+  void initState() {
+    super.initState();
+    _firstNameController = TextEditingController();
+    _lastNameController = TextEditingController();
+    _emailController = TextEditingController();
+    _passwordController = TextEditingController();
+    _confirmPasswordController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _firstNameController.dispose();
+    _lastNameController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    _confirmPasswordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,14 +75,22 @@ class SignupScreen extends StatelessWidget {
                 ),
                 child: Column(
                   children: [
-                    const CustomInputField(
+                    CustomInputField(
                       icon: Icons.person_outline,
-                      hintText: 'Enter your full name',
+                      hintText: 'Enter your first name',
+                      controller: _firstNameController,
                     ),
                     const SizedBox(height: 16),
-                    const CustomInputField(
+                    CustomInputField(
+                      icon: Icons.person_outline,
+                      hintText: 'Enter your last name',
+                      controller: _lastNameController,
+                    ),
+                    const SizedBox(height: 16),
+                    CustomInputField(
                       icon: Icons.email_outlined,
                       hintText: 'Enter your email',
+                      controller: _emailController,
                     ),
                     const SizedBox(height: 16),
                     const CustomInputField(
@@ -57,19 +98,21 @@ class SignupScreen extends StatelessWidget {
                       hintText: 'Enter your phone number',
                     ),
                     const SizedBox(height: 16),
-                    const CustomInputField(
+                    CustomInputField(
                       icon: Icons.lock_outline,
                       hintText: 'Create a password',
                       obscureText: true,
+                      controller: _passwordController,
                     ),
                     const SizedBox(height: 16),
-                    const CustomInputField(
+                    CustomInputField(
                       icon: Icons.lock_outline,
                       hintText: 'Confirm your password',
                       obscureText: true,
+                      controller: _confirmPasswordController,
                     ),
                     const SizedBox(height: 16),
-      
+
                     Container(
                       padding: const EdgeInsets.symmetric(horizontal: 16),
                       decoration: BoxDecoration(
@@ -94,9 +137,9 @@ class SignupScreen extends StatelessWidget {
                         onChanged: (value) {},
                       ),
                     ),
-      
+
                     const SizedBox(height: 24),
-      
+
                     SizedBox(
                       width: double.infinity,
                       height: 48,
@@ -117,15 +160,13 @@ class SignupScreen extends StatelessWidget {
                           ),
                         ),
                       ),
-      
-                    )
-      
+                    ),
                   ],
                 ),
               ),
-      
+
               const SizedBox(height: 24),
-      
+
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: const [
@@ -142,35 +183,34 @@ class SignupScreen extends StatelessWidget {
                   ),
                 ],
               ),
-      
+
               const SizedBox(height: 24),
-      
             ],
           ),
-        )
+        ),
       ),
     );
   }
 }
 
-
-
-
 class CustomInputField extends StatelessWidget {
   final IconData icon;
   final String hintText;
   final bool obscureText;
+  final TextEditingController? controller;
 
   const CustomInputField({
     super.key,
     required this.icon,
     required this.hintText,
     this.obscureText = false,
+    this.controller,
   });
 
   @override
   Widget build(BuildContext context) {
     return TextField(
+      controller: controller,
       obscureText: obscureText,
       decoration: InputDecoration(
         prefixIcon: Icon(icon, color: Color(0xFFA1A1A1)),
@@ -178,7 +218,10 @@ class CustomInputField extends StatelessWidget {
         hintStyle: const TextStyle(color: Color(0xFFA1A1A1)),
         filled: true,
         fillColor: Colors.white,
-        contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 14),
+        contentPadding: const EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 14,
+        ),
         enabledBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(12),
           borderSide: const BorderSide(color: Color(0xFFE5E5E5)),
@@ -191,5 +234,3 @@ class CustomInputField extends StatelessWidget {
     );
   }
 }
-
-
