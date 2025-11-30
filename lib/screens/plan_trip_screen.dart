f6d17377717e21aab744891606bec8c0faa822c7
Muhammad Bilal Farid <bilaldrosh1@gmail.com>
2025-11-30 12:30:00 +0500

    feat: Add PlanTripScreen skeleton

---
+++ lib/screens/plan_trip_screen.dart
@@ -0,0 +1,37 @@
+import 'package:swift_trip_app/theme/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class PlanTripScreen extends StatefulWidget {
+  const PlanTripScreen({super.key});
+
+  @override
+  State<PlanTripScreen> createState() => _PlanTripScreenState();
+}
+
+class _PlanTripScreenState extends State<PlanTripScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: AppColors.textPrimary),
+          onPressed: () => Navigator.pop(context),
+        ),
+        title: const Text(
+          'Plan Your Trip',
+          style: TextStyle(
+            color: AppColors.textPrimary,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Text("Plan Trip Content", style: TextStyle(color: AppColors.textPrimary)),
+      ),
+    );
+  }
+}
