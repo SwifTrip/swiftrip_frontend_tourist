8e98299c7e6b3626fd149be3e1abd82f6057374a
Muhammad Bilal Farid <bilaldrosh1@gmail.com>
2025-12-18 11:28:15 +0500

    feat: add logo and animated success icon to PasswordUpdatedScreen

--- lib/screens/password_updated_screen.dart
+++ lib/screens/password_updated_screen.dart
@@ -9,7 +9,32 @@ class PasswordUpdatedScreen extends StatefulWidget {
   State<PasswordUpdatedScreen> createState() => _PasswordUpdatedScreenState();
 }
 
-class _PasswordUpdatedScreenState extends State<PasswordUpdatedScreen> {
+class _PasswordUpdatedScreenState extends State<PasswordUpdatedScreen> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _bounceAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    )..repeat(reverse: true);
+
+    _bounceAnimation = Tween<double>(begin: 0, end: -10).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeInOut,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,7 +46,60 @@ class _PasswordUpdatedScreenState extends State<PasswordUpdatedScreen> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               const Spacer(),
-              // Content will be added in subsequent commits
+              // Logo
+              Center(
+                child: Container(
+                  width: 150,
+                  height: 150,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.2),
+                        blurRadius: 20,
+                        offset: const Offset(0, 10),
+                      ),
+                    ],
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(12),
+                    child: Image.asset(
+                      'lib/assets/logo.png',
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 32),
+              // Success Icon
+              AnimatedBuilder(
+                animation: _bounceAnimation,
+                builder: (context, child) {
+                  return Transform.translate(
+                    offset: Offset(0, _bounceAnimation.value),
+                    child: Container(
+                      padding: const EdgeInsets.all(24),
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: const Color(0xFF1F362D),
+                        border: Border.all(color: Colors.white.withOpacity(0.1)),
+                        boxShadow: [
+                          BoxShadow(
+                            color: AppColors.accent.withOpacity(0.2),
+                            blurRadius: 30,
+                            spreadRadius: 5,
+                          ),
+                        ],
+                      ),
+                      child: const Icon(
+                        Icons.check_circle_outline,
+                        size: 80,
+                        color: AppColors.accent,
+                      ),
+                    ),
+                  );
+                },
+              ),
               const Spacer(),
             ],
           ),
