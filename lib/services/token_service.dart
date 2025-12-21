123cf29fd90877ca16f1656ebbec526d322d39dd
Muhammad Bilal Farid <bilaldrosh1@gmail.com>
2025-12-21 10:15:00 +0500

    feat(services): extend TokenService to persist user data

--- lib/services/token_service.dart
+++ lib/services/token_service.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
+import '../models/user_model.dart';
 
 class TokenService {
   static const String _tokenKey = 'auth_token';
+  static const String _userKey = 'auth_user';
 
   // Save token to local storage
   static Future<void> saveToken(String token) async {
@@ -19,6 +22,28 @@ class TokenService {
   static Future<void> removeToken() async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.remove(_tokenKey);
+  }
+
+  // Save user data to local storage
+  static Future<void> saveUser(UserModel user) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(_userKey, jsonEncode(user.toJson()));
+  }
+
+  // Get user data from local storage
+  static Future<UserModel?> getUser() async {
+    final prefs = await SharedPreferences.getInstance();
+    final userStr = prefs.getString(_userKey);
+    if (userStr != null) {
+      return UserModel.fromJson(jsonDecode(userStr));
+    }
+    return null;
+  }
+
+  // Remove user data from local storage
+  static Future<void> removeUser() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove(_userKey);
   }
 
   // Check if token exists
